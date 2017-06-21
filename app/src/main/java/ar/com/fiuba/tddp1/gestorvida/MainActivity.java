30049b9e582a33b90a8f329fed016748a018589f
hugo <hechavar@gmail.com>
2017-06-20 23:47:16 -0300

    agrego bibliotecas para conectar al server

--- app/src/main/java/ar/com/fiuba/tddp1/gestorvida/MainActivity.java
+++ app/src/main/java/ar/com/fiuba/tddp1/gestorvida/MainActivity.java
@@ -13,6 +13,14 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import ar.com.fiuba.tddp1.gestorvida.actividades.ActividadesFragment;
 
@@ -151,5 +159,33 @@ public class MainActivity extends AppCompatActivity
         fragmentManager.beginTransaction()
                         .replace(R.id.contenedor, fragment)
                         .commit();
+    }
+
+    public void sendTestHttpRequest() {
+        final TextView mTextView = (TextView) findViewById(R.id.textView2);
+
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://127.0.0.1:8080/pasarela/";
+        mTextView.setText("Realizo request .. ");
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+
+                            mTextView.setText("Response is: "+ response);
+                        }
+                    }, new Response.ErrorListener() {
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+                                    mTextView.setText("That didn't work!");
+                                }
+                });
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
     }
 }
--- app/src/main/java/ar/com/fiuba/tddp1/gestorvida/actividades/RecyclerFragment.java
+++ app/src/main/java/ar/com/fiuba/tddp1/gestorvida/actividades/RecyclerFragment.java
@@ -52,6 +52,8 @@ public abstract class RecyclerFragment extends Fragment /*implements AdapterView
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
