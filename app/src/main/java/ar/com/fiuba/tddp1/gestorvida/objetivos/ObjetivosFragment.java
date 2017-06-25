3c59e73ff4b3228acb59a9818782be26f339ace2
juanmafc <juanmafc25@gmail.com>
2017-06-24 22:09:00 -0300

    ObjetivosFragment ahora usa el Perfil

--- app/src/main/java/ar/com/fiuba/tddp1/gestorvida/objetivos/ObjetivosFragment.java
+++ app/src/main/java/ar/com/fiuba/tddp1/gestorvida/objetivos/ObjetivosFragment.java
@@ -24,39 +24,12 @@ public class ObjetivosFragment extends RecyclerFragment {
 
     @Override
     protected void configureAdapter() {
-
-
-        //POR AHORA CARGO LOS OBJETIVOS ACA
-        ArrayList<Objetivo> listaDeObjetivos = new ArrayList<>();
-
-        listaDeObjetivos.add(new Objetivo("Obj1"));
-
-        Objetivo objetivoConActividades = new Objetivo("Obj2");
-        objetivoConActividades.agregarActividad(new Actividad("Actividad1"));
-
-        Actividad actividadCompletada = new Actividad("Actividad2");
-        actividadCompletada.completar();
-        objetivoConActividades.agregarActividad(actividadCompletada);
-
-        actividadCompletada = new Actividad("Actividad3");
-        actividadCompletada.completar();
-        objetivoConActividades.agregarActividad(actividadCompletada);
-
-        objetivoConActividades.agregarActividad(new Actividad("Actividad4"));
-        listaDeObjetivos.add(objetivoConActividades);
-
-        listaDeObjetivos.add(new Objetivo("Obj3"));
-        listaDeObjetivos.add(new Objetivo("Obj4"));
-        listaDeObjetivos.add(new Objetivo("Obj5"));
-        listaDeObjetivos.add(new Objetivo("Obj6"));
-        listaDeObjetivos.add(new Objetivo("Obj7"));
-        listaDeObjetivos.add(new Objetivo("Obj8"));
-        listaDeObjetivos.add(new Objetivo("Obj9"));
-        listaDeObjetivos.add(new Objetivo("Obj10"));
-
-        ObjetivoAdapter objetivoAdapter = new ObjetivoAdapter(listaDeObjetivos, getActivity());
-        //DESPUES LOS OBJETIVOS SE VAN A SACAR DEL PERFIL
-        //ObjetivoAdapter objetivoAdapter = new ObjetivoAdapter(Perfil.getObjetivos(), getActivity());
+        ObjetivoAdapter objetivoAdapter = new ObjetivoAdapter(Perfil.getObjetivos(), getActivity());
         this.setConfiguredAdapter(objetivoAdapter);
+    }
+
+    @Override
+    protected void goToAgregarElemento() {
+
     }
 }
