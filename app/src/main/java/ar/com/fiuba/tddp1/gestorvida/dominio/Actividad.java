5a92a6721667d5ff2edf5584b61eb7bdaf51927c
juanmafc <juanmafc25@gmail.com>
2017-06-25 10:54:18 -0300

    Actividad guarda fecha de inicio y fin

--- app/src/main/java/ar/com/fiuba/tddp1/gestorvida/dominio/Actividad.java
+++ app/src/main/java/ar/com/fiuba/tddp1/gestorvida/dominio/Actividad.java
@@ -10,6 +10,9 @@ public class Actividad {
     private String descripcion;
     private boolean estaCompletada;
 
+    private Fecha fechaInicio = null;
+    private Fecha fechaFin = null;
+
     public Actividad(String nombre) {
         this.nombre = nombre;
         this.estaCompletada = false;
@@ -18,6 +21,17 @@ public class Actividad {
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
+
+    public void setFechaInicio(Fecha fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public void setFechaFin(Fecha fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+
+
 
     public boolean estaCompletada() {
         return this.estaCompletada;
---
+++ app/src/main/java/ar/com/fiuba/tddp1/gestorvida/dominio/Fecha.java
@@ -0,0 +1,17 @@
+package ar.com.fiuba.tddp1.gestorvida.dominio;
+
+/**
+ * Created by User on 25/06/2017.
+ */
+
+public class Fecha {
+    public String dia;
+    public String mes;
+    public String anio;
+
+    public Fecha(String dia, String mes, String anio) {
+        this.dia = dia;
+        this.mes = mes;
+        this.anio = anio;
+    }
+}
