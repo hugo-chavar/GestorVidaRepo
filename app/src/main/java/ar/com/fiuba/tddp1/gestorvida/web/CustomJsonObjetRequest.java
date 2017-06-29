151ec95bcd9b6c75b825fae49628ef533724c2e7
hugo <hechavar@gmail.com>
2017-06-29 00:25:08 -0300

    Separo la logica repetida

--- app/src/main/java/ar/com/fiuba/tddp1/gestorvida/web/CustomJsonObjetRequest.java
+++ app/src/main/java/ar/com/fiuba/tddp1/gestorvida/web/CustomJsonObjetRequest.java
@@ -4,23 +4,15 @@ package ar.com.fiuba.tddp1.gestorvida.web;
 import android.util.Log;
 
 import com.android.volley.AuthFailureError;
-import com.android.volley.NetworkError;
 import com.android.volley.NetworkResponse;
-import com.android.volley.NoConnectionError;
-import com.android.volley.ParseError;
 import com.android.volley.Response;
-import com.android.volley.ServerError;
-import com.android.volley.TimeoutError;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.HttpHeaderParser;
 import com.android.volley.toolbox.JsonObjectRequest;
 
 import org.json.JSONObject;
 
-import java.util.HashMap;
 import java.util.Map;
-
-import ar.com.fiuba.tddp1.gestorvida.dominio.Perfil;
 
 public class CustomJsonObjetRequest extends JsonObjectRequest {
 
@@ -37,58 +29,12 @@ public class CustomJsonObjetRequest extends JsonObjectRequest {
                     }
                 },
                 new Response.ErrorListener() {
-                    private Pair<Integer, String> getError(VolleyError error) {
-                        String errorDesc;
-                        Integer codError = 0;
-
-                        if (error instanceof TimeoutError || error instanceof NoConnectionError) {
-
-                            errorDesc = "El servidor tardo mucho en responder";
-
-                        } else if (error instanceof AuthFailureError) {
-
-                            errorDesc = "Error de autenticacion";
-
-                        } else if (error instanceof ServerError) {
-
-                            codError = error.networkResponse.statusCode;
-
-                            switch (codError) {
-                                case 409:
-                                    errorDesc = "Nombre de usuario ya existe";
-                                    break;
-                                case 400:
-                                    errorDesc = "No se recibieron todos los parametros";
-                                    break;
-                                case 404:
-                                    errorDesc = "La url invocada no corresponde a un servicio valido";
-                                    break;
-                                default:
-                                    errorDesc = new String(error.networkResponse.data);
-
-                            }
-                        } else if (error instanceof NetworkError) {
-
-                            errorDesc = "Error de red";
-
-                        } else if (error instanceof ParseError) {
-
-                            errorDesc = "Error de parseo";
-                            error.printStackTrace();
-
-                        } else {
-
-                            errorDesc = "Error desconocido. ";
-
-                        }
-                        return new Pair<Integer, String>(codError, errorDesc);
-                    }
 
                     @Override
                     public void onErrorResponse(VolleyError error) {
                         Pair<Integer, String> errorDetail;
 
-                        errorDetail = getError(error);
+                        errorDetail = RequestHelper.getError(error);
 
                         listener.onRequestError(errorDetail.t, errorDetail.u);
 
@@ -112,15 +58,8 @@ public class CustomJsonObjetRequest extends JsonObjectRequest {
 
     @Override
     public Map getHeaders() throws AuthFailureError {
-        Map headers = new HashMap<>();
-        String auth = Perfil.token;
-        headers.put("Content-Type", "application/json");
-        if (Perfil.token != null) {
-            headers.put("Authorization", auth);
-        }
 
-
-        return headers;
+        return RequestHelper.getHeaders();
     }
 
     /*@Override
--- app/src/main/java/ar/com/fiuba/tddp1/gestorvida/web/ResponseListener.java
+++ app/src/main/java/ar/com/fiuba/tddp1/gestorvida/web/ResponseListener.java
@@ -1,14 +1,7 @@
 package ar.com.fiuba.tddp1.gestorvida.web;
 
-/**
- * Created by hugo on 22/06/17.
- */
 
 public interface ResponseListener {
-
-    //void onRequestCompleted(JSONObject response);
-
-    //void onRequestCompleted(JSONArray response);
 
     void onRequestCompleted(Object response);
 
