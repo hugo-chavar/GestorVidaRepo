64ee8d6256063a5d0a0e50e55a55c679e156405b
hugo <hechavar@gmail.com>
2017-07-01 14:06:32 -0300

    Elimino comentarios

--- app/src/main/java/ar/com/fiuba/tddp1/gestorvida/actividades/ActividadesListener.java
+++ app/src/main/java/ar/com/fiuba/tddp1/gestorvida/actividades/ActividadesListener.java
@@ -49,56 +49,5 @@ public class ActividadesListener implements ResponseListener {
         Toast.makeText(context, error, Toast.LENGTH_LONG).show();
     }
 
-    /*private Actividad toActividad(JSONObject jsonObject) throws JSONException {
 
-        Actividad actividad = new Actividad(jsonObject.getString("nombre"));
-        actividad.setDescripcion(jsonObject.getString("descripcion"));
-        actividad.setFoto(jsonObject.getString("foto"));
-        actividad.setId(jsonObject.getString("_id"));
-        actividad.setFechaInicio(new Fecha(jsonObject.getString("fechaInicio")));
-        actividad.setFechaFin(new Fecha(jsonObject.getString("fechaFin")));
-        actividad.setFechaRecordatorio(new Fecha(jsonObject.getString("recordatorio")));
-        actividad.setPeriodicidad(Integer.parseInt(jsonObject.getString("periodicidad")));
-        actividad.setTiempoEstimado(String.valueOf(jsonObject.getInt("estimacion")), "0");
-
-        JSONArray st = jsonObject.getJSONArray("categorias");
-
-        //TODO: aca ademas deberia parsearse el color
-        Set<Etiqueta> etiquetas = new HashSet<Etiqueta>();
-        for (int i = 0; i < st.length(); i++) {
-            etiquetas.add(new Etiqueta(st.getString(i), Color.BLACK));// TODO: sacar el color BLACK hardcodeado y poner el del server
-        }
-        actividad.setEtiquetas(etiquetas);
-
-        boolean completada = jsonObject.getBoolean("completada");
-        if (completada) {
-            actividad.completar();
-        }
-
-        st = jsonObject.getJSONArray("categorias");
-        Set<String> participantes = new HashSet<String>();
-        for (int i = 0; i < st.length(); i++) {
-            participantes.add(st.getString(i));
-        }
-
-        actividad.setParticipantes(participantes);
-
-        JSONArray benef = jsonObject.getJSONArray("beneficios");
-
-        for (int i = 0; i < benef.length(); i++) {
-            Beneficio beneficio = new Beneficio();
-            JSONObject jo = benef.getJSONObject(i);
-            beneficio.setDescripcion(jo.getString("descripcion"));
-            beneficio.setDescuento(jo.getDouble("descuento"));
-            beneficio.setPrecio(jo.getDouble("precio"));
-            actividad.addBeneficio(beneficio);
-        }
-
-
-
-        return actividad;
-
-
-
-    }*/
 }
