3e51bba72d0bcfc58616b9d877b1d2afeeba6a7c
juanmafc <juanmafc25@gmail.com>
2017-07-02 20:02:08 -0300

    Un archivo que faltó

--- app/src/main/java/ar/com/fiuba/tddp1/gestorvida/actividades/DetalleActividadAgregarParticipantes.java
+++ app/src/main/java/ar/com/fiuba/tddp1/gestorvida/actividades/DetalleActividadAgregarParticipantes.java
@@ -6,11 +6,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import ar.com.fiuba.tddp1.gestorvida.MainActivity;
 import ar.com.fiuba.tddp1.gestorvida.R;
 import ar.com.fiuba.tddp1.gestorvida.comunes.FragmentLoader;
 import ar.com.fiuba.tddp1.gestorvida.contactos.AgregarParticipantesFragment;
+import ar.com.fiuba.tddp1.gestorvida.dominio.Contacto;
 
 /**
  * Created by User on 02/07/2017.
@@ -24,6 +27,7 @@ public class DetalleActividadAgregarParticipantes extends DetalleActividadFragme
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.detalle_actividad_agregar_participantes_layout, container, false);
         init(view);
+
 
         this.fabAgregarParticipante = (FloatingActionButton) view.findViewById(R.id.fabAgregarParticipante);
         this.fabAgregarParticipante.setOnClickListener(new View.OnClickListener() {
